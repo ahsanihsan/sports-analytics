a67011db791c5c79d4de8106f94ed0500ba9b072
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2020-11-18 21:47:28 +0500

    Who will win and dashboard finalised

--- src/views/Predicitons/WhoWillWin.js
+++ src/views/Predicitons/WhoWillWin.js
@@ -3,12 +3,24 @@ import { Button, Card, Col, notification, Row, Select, Spin } from "antd";
 import { MatchTypes, Months, Teams } from "../../helpers/Teams";
 import constants from "../../helpers/constants";
 import { post } from "../../helpers/request";
+import { CWidgetDropdown } from "@coreui/react";
+import ChartBarSimple from "../charts/ChartBarSimple";
+import ChartLineSimple from "../charts/ChartLineSimple";
+import { CChartBar } from "@coreui/react-chartjs";
 
 export default class WhoWillWin extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: false,
+      team_a: "Pakistan",
+      team_b: "India",
+      month: "January",
+      match_type: "ODI",
+      city: "Karachi",
+      toss_won: "Pakistan",
+      toss_decision: "bat",
+      venue: "National Stadium",
     };
   }
 
@@ -34,6 +46,18 @@ export default class WhoWillWin extends Component {
       venue,
       toss_won,
     });
+
+    let teamScore = await post(constants.URL.PREDICTION.SCORE_OF_TEAMS, {
+      team_a,
+      team_b,
+      city,
+      month,
+      match_type,
+      toss_decision,
+      venue,
+      toss_won,
+    });
+
     let runRate = await post(constants.URL.PREDICTION.RUN_RATE, {
       match_type,
       batting_team:
@@ -45,9 +69,17 @@ export default class WhoWillWin extends Component {
     });
 
     if (whoWillWin && runRate) {
+      let totalScore = 0;
+      let runRateRound = [];
+      runRate.data.prediction.map((item) => {
+        runRateRound.push(Math.round(item));
+        const perTenOvers = item * 10;
+        totalScore += perTenOvers;
+      });
       this.setState({
         whoWillWin: whoWillWin.data,
         runRate: runRate.data,
+        totalScore: Math.round(totalScore),
         isLoading: false,
         predicted: true,
       });
@@ -78,6 +110,7 @@ export default class WhoWillWin extends Component {
               <div>
                 <label>Team A</label>
                 <Select
+                  value={this.state.team_a}
                   onChange={(team_a) => this.setState({ team_a })}
                   placeholder="Select Team A"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -88,6 +121,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>Team B</label>
                 <Select
+                  value={this.state.team_b}
                   onChange={(team_b) => this.setState({ team_b })}
                   placeholder="Select Team B"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -100,6 +134,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>Month</label>
                 <Select
+                  value={this.state.month}
                   onChange={(month) => this.setState({ month })}
                   placeholder="Select Month"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -112,6 +147,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>Match Type</label>
                 <Select
+                  value={this.state.match_type}
                   onChange={(match_type) => this.setState({ match_type })}
                   placeholder="Select Match Type"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -124,6 +160,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>City</label>
                 <Select
+                  value={this.state.city}
                   onChange={(city) => this.setState({ city })}
                   placeholder="Select City"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -135,6 +172,7 @@ export default class WhoWillWin extends Component {
                 <div style={{ marginTop: 10 }}>
                   <label>Toss Won</label>
                   <Select
+                    value={this.state.toss_won}
                     onChange={(toss_won) => this.setState({ toss_won })}
                     placeholder="Select Toss Winning Team"
                     style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -151,6 +189,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>Toss Decision</label>
                 <Select
+                  value={this.state.toss_decision}
                   onChange={(toss_decision) => this.setState({ toss_decision })}
                   placeholder="Select Toss Decision"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -162,6 +201,7 @@ export default class WhoWillWin extends Component {
               <div style={{ marginTop: 10 }}>
                 <label>Venue</label>
                 <Select
+                  value={this.state.venue}
                   onChange={(venue) => this.setState({ venue })}
                   placeholder="Select Venue"
                   style={{ width: "100%", borderRadius: 10, marginTop: 5 }}
@@ -183,14 +223,69 @@ export default class WhoWillWin extends Component {
           </Col>
           <Col span={15}>
             <Card
-              title="Our Prediction"
+              title="Winner Team Prediction"
               style={{ width: "100%", borderRadius: 10 }}
             >
               {this.state.isLoading ? (
                 <Spin />
               ) : this.state.predicted ? (
                 <div>
-                  <p>Who Will Win</p>
+                  <Row gutter={10}>
+                    <Col span={12}>
+                      <CWidgetDropdown
+                        color="gradient-warning"
+                        header={this.state.whoWillWin.prediction}
+                        text="Winner Team"
+                        footerSlot={
+                          <ChartLineSimple
+                            dataPoints={this.state.runRate.prediction}
+                            className="mt-3"
+                            style={{ height: "70px" }}
+                            backgroundColor="rgba(255,255,255,.2)"
+                            options={{
+                              elements: { line: { borderWidth: 2.5 } },
+                            }}
+                            pointHoverBackgroundColor="warning"
+                            label="Run Rate"
+                            labels="runrate"
+                          />
+                        }
+                      />
+                    </Col>
+                    <Col span={12}>
+                      <CWidgetDropdown
+                        color="gradient-primary"
+                        header={this.state.totalScore}
+                        text="Team Total Score"
+                        footerSlot={
+                          <ChartBarSimple
+                            dataPoints={this.state.runRate.prediction}
+                            className="mt-3 mx-3"
+                            style={{ height: "70px" }}
+                            backgroundColor="rgb(250, 250, 250, 0.5)"
+                            label="Run Rate"
+                            labels="runrate"
+                          />
+                        }
+                      />
+                    </Col>
+                  </Row>
+                  <CChartBar
+                    type="bar"
+                    datasets={[
+                      {
+                        label: "Run Rate",
+                        backgroundColor: "#f87979",
+                        data: this.state.runRate.prediction,
+                      },
+                    ]}
+                    labels={["10", "20", "30", "40", "50"]}
+                    options={{
+                      tooltips: {
+                        enabled: true,
+                      },
+                    }}
+                  />
                 </div>
               ) : (
                 <div>Please select values to continue.</div>
